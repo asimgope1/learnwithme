fc506ca9c2d152a8ebe8291f5845cbe697057e86
asimgope <asimgope53@gmail.com>
2024-05-21 22:30:02 +0530

    first commit

---
+++ app/(tabs)/BlueScreen.tsx
@@ -0,0 +1,18 @@
+import { View, Text } from "react-native";
+import React from "react";
+
+const BlueScreen = () => {
+	return (
+		<View
+			style={{
+				flex: 1,
+				justifyContent: "center",
+				alignItems: "center",
+				backgroundColor: "blue",
+			}}>
+			<Text>BlueScreen</Text>
+		</View>
+	);
+};
+
+export default BlueScreen;
